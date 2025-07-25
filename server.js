53af65e33d2de5829f2ee3d1a69f9b5cdd151819
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-07-25 16:08:50 -0700

    Add db config

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB Connection Successful!'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
