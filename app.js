86f3dc3b0c4ae7747e1f34e7ffb191d3d349ef72
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-08-07 13:13:40 -0700

    global Error Handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const userRouter = require('./routes/UserRouters');
 const tourRouter = require('./routes/tourRouters');
+const { status } = require('express/lib/response');
 
 const app = express();
 // MIDDLEWARES
@@ -20,15 +21,27 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-// app.use('/api/v1/tours', tourRouter);
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all(/.*/, (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
 
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    messsage: err.message,
+  });
 });
 
 module.exports = app;
