ce23a029f4aa96eeed00aa13f54ec71457354c87
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-08-07 17:19:00 -0700

    Safty net

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHTEXCEPTION! SHUTTING DOWN .....⏳');
+  console.log(err.name, err.message);
+  process.exit(1); //uncleanstate
+
+});
+
 dotenv.config();
 const app = require('./app');
 
@@ -12,8 +19,16 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log('DB Connection Successful!'));
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runing on port ${port}`);
 });
 
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN .....⏳');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
+console.log(X);
