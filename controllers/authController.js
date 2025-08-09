c32475a8ee3740b919453374eb56daaf65d31a01
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-08-09 15:40:32 -0700

    Adding userModel and authControllers

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const { escape } = require('validator');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    date: {
+      user: newUser,
+    },
+  });
+});
---
+++ models/userModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User must have a name'],
+    unique: [true, 'User name must be unique'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: [true, 'Email must be unique'],
+    trim: true,
+    // i dont know about this
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password us required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/.test(
+          value,
+        );
+      },
+      message:
+        'Password must have one Uppercase letter, One lowercase letter, One number and one speciel character',
+    },
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // Only works on CREATE and SAVE not in UPDATE
+      // TODO: only update with save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
+  },
+});
+
+userSchema.pre('save', async (req, res, next) => {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
